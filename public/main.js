563f17f643554cb1682086a35af6f9c4db50c9e9
vsheo <vireshsheoratan@gmail.com>
2025-02-12 15:06:39 +0100

    #5 bounce op cirkels

--- public/main.js
+++ public/main.js
@@ -2,36 +2,40 @@
 cirkels = document.querySelectorAll(".profile-bubble")
 
 // genereer nieuwe x & y om de 2s
-let cirkelAnimation = setInterval(setPropertyXY, 2000);
+setPropertyXY()
+let cirkelAnimation = setInterval(setPropertyXY, 10000);
 
 // event listener op elke cirkel
-cirkels.forEach(cirk => {
+cirkels.forEach(cirkel => {
     // stop de animatie bij een hover
-    cirk.addEventListener("mouseover", () => {
+    cirkel.addEventListener("click", () => {
         clearInterval(cirkelAnimation)
+        console.log("ai wroko")
+        // cirk.style.setProperty('--size-circle', 20 + 'em')
+        cirkel.classList.toggle("open")
     });
 
-    // start de animatie opniew wanneer er geen hover is
-    cirk.addEventListener("mouseleave", () => {
-        cirkelAnimation = setInterval(setPropertyXY, 2000);
-    });
+    // // start de animatie opniew wanneer er geen hover is
+    // cirk.addEventListener("mouseleave", () => {
+    //     cirkelAnimation = setInterval(setPropertyXY, 1000);
+    // });
 });
 
-
-// https://www.w3schools.com/js/js_random.asp
-// genereer een random getal tussen 0 en 100
-function random100() {
-    return Math.floor(Math.random() * 30);
-}
 
 // genereer een random x & y voor elke cirkel
 function setPropertyXY() {
     cirkels.forEach(cirkel => {
-        x = random100() + "dvh";
-        y = random100() + "dvw";
+        // https://www.w3schools.com/js/js_random.asp
+        // genereer een random getal tussen 0 en 100
+        durX = Math.floor(Math.random() * 20);
+        durY = Math.floor(Math.random() * 20);
+
+        // z-index
+        z = Math.floor(Math.random() * 100);
     
-        cirkel.style.setProperty('--x', x)
-        cirkel.style.setProperty('--y', y)
+        cirkel.style.setProperty('--durX', durX)
+        cirkel.style.setProperty('--durY', durY)
+        cirkel.style.setProperty('--z', z)
     });
 }
 
